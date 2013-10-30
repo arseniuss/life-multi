384358b3feaec615869e929d679e60d62023814a
Armands Skolmeisters (Arseniuss) <arseniuss@arseniuss.id.lv>
2013-10-30 17:13:34 +0200

    Added platform detection

--- include/config.h
+++ include/config.h
@@ -7,6 +7,21 @@
 
 #pragma once
 
+#define PLATFORM_WINDOWS                1
+#define PLATFORM_MAC                    2
+#define PLATFORM_UNIX                   3
+
+//nosaka operētājsistēmu
+#if defined(_WIN32)
+#    define PLATFORM PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+#    define PLATFORM PLATFORM_MAC
+#elif defined(__unix)
+#    define PLATFORM PLATFORM_UNIX
+#else
+#    error "Unknown environment!"
+#endif
+
 #define FPS             32
 
 #define APP_NAME                        "Life Game Multiplayer"
